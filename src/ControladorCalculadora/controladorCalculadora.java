e8c8805b83d15a85aa3696e74a3e7dcfb5722ada
ErickFRD <erick.reyesd1416@gmail.com>
2026-08-04 14:43:43 -0600

    Se crea la clase controlador con sus variables asignadas

--- src/ControladorCalculadora/controladorCalculadora.java
+++ src/ControladorCalculadora/controladorCalculadora.java
@@ -9,5 +9,20 @@ package ControladorCalculadora;
  * @author Student
  */
 public class controladorCalculadora {
+    private double valor1;
+    private double valor2;
+    private String operacion;
+    private boolean operacionElegida;
+    private boolean esperarValor2;
+    private boolean resultadoMostrado;
     
+    public controladorCalculadora(){
+        valor1=0;
+        valor2=0;
+        operacion="";
+        
+        operacionElegida=false;
+        esperarValor2=false;
+        resultadoMostrado=false;
+    }
 }
